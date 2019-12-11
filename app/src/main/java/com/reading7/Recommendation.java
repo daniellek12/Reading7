590bbd5939f657ec7104a132a9f0cf433408b17c
yonitb <toybenshlak@campus.technion.ac.il>
2019-12-11 14:36:32 +0200

    Updated the Recommendation object

--- app/src/main/java/com/reading7/Recommendation.java
+++ app/src/main/java/com/reading7/Recommendation.java
@@ -3,10 +3,12 @@ package com.reading7;
 public class Recommendation {
     private String user_id;
     private String book_id;
+    private double similarity_rating;
 
-    public Recommendation(String user_id, String book_id){
+    public Recommendation(String user_id, String book_id, double rating){
         this.user_id = user_id;
         this.book_id = book_id;
+        this.similarity_rating = rating;
     }
 
     public String getBook_id() {
@@ -17,11 +19,21 @@ public class Recommendation {
         return user_id;
     }
 
+    public double getSimilarity_rating() {
+        return similarity_rating;
+    }
+
+    //these shouldn't be used, idealy new recommendations will be written from server side
+
     public void setBook_id(String book_id) {
         this.book_id = book_id;
     }
 
     public void setUser_id(String user_id) {
         this.user_id = user_id;
+    }
+
+    public void setSimilarity_rating(double similarity_rating) {
+        this.similarity_rating = similarity_rating;
     }
 }
