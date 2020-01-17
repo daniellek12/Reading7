84d49292ce5e21480bbcb8b53c95c7b331309d88
elizabethlan <57498464+elizabethlan@users.noreply.github.com>
2020-01-17 20:17:47 +0200

    added input check when choosing a shelf to add to

--- app/src/main/java/com/reading7/AddToShelfDialog.java
+++ app/src/main/java/com/reading7/AddToShelfDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,11 +21,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.reading7.Objects.Shelf;
+
+import java.util.ArrayList;
 
 public class AddToShelfDialog extends AppCompatDialogFragment {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private View view;
+    private ArrayList<String> shelfNames = new ArrayList<>();
 
     @NonNull
     @Override
@@ -33,6 +38,8 @@ public class AddToShelfDialog extends AppCompatDialogFragment {
 
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+
+        getUserShelves();
 
         final String book_title = getArguments().getString("book_title");
 
@@ -45,6 +52,18 @@ public class AddToShelfDialog extends AppCompatDialogFragment {
             public void onClick(View view) {
                 final String shelf_name = shelfText.getText().toString().trim();
 
+                if (shelf_name.isEmpty()){
+                    String error = shelf_name + "עדיין לא בחרת מדף...";
+                    Toast.makeText(getContext(), error, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (!shelfNames.contains(shelf_name)){
+                    String error = "אופס! אין לך מדף ששמו " + shelf_name;
+                    Toast.makeText(getContext(), error, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 CollectionReference requestCollectionRef = db.collection("Users")
                         .document(mAuth.getCurrentUser().getEmail()).collection("Shelves");
                 Query requestQuery = requestCollectionRef.whereEqualTo("shelf_name", shelf_name);
@@ -55,6 +74,7 @@ public class AddToShelfDialog extends AppCompatDialogFragment {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 DocumentReference shelfRef = document.getReference();
                                 shelfRef.update("book_names", FieldValue.arrayUnion(book_title));
+                                //Toast.makeText(AddToShelfDialog.this.getContext(),  "הספר נוסף למדף " + shelf_name, Toast.LENGTH_SHORT).show();
                             }
                         }
                     }
@@ -73,5 +93,20 @@ public class AddToShelfDialog extends AppCompatDialogFragment {
 
         builder.setView(view);
         return builder.create();
+    }
+
+    private void getUserShelves(){
+        db.collection("Users").document(mAuth.getCurrentUser().getEmail())
+                .collection("Shelves").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    shelfNames.clear();
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        shelfNames.add(doc.toObject(Shelf.class).getShelf_name());
+                    }
+                }
+            }
+        });
     }
 }
